00d1eb6d904b7c8555d5c5b68236cba467498a6d
I Ismail <iismail@gmail.com>
2017-08-29 11:40:56 -0400

    Turn OFF to open the relay control and vice versa

--- gpio7.py
+++ gpio7.py
@@ -3,11 +3,13 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
+GPIO.output(7,False)
+time.sleep(2)
 GPIO.output(7,True)
-time.sleep(1)
 
-GPIO.cleanup()
 
